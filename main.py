cb2019729c63a6e9ad67a580089066beeed363dc
FoxtrotPapa_25 <Francois25@users.noreply.github.com>
2022-12-16 08:54:37 +0100

    reorganisation code

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from machine import Pin, PWM, Timer, TouchPad
-from time import sleep_ms, sleep
 import time, utime
 import musiques
-from musiques import liste_music
 import random
+from machine import Pin, PWM, Timer, TouchPad
+from time import sleep_ms, sleep
+from musiques import liste_music
 
 
-# allumage des leds à la demande
+# allumage des leds à la demande --> ok
 # 18h00 allumage des les clignotantes
 # 22h extinction des leds clignotantes
-# jouer des champs de Noel à la demande
+# jouer des champs de Noel à la demande --> ok
 # jouer des champts de Noel à heure précise
-# allumage de la led du jour pour trouver le présent par appui sur bouton
+# allumage de la led du jour pour trouver le cadeau par appui sur bouton --> ok
+
+        
+TOUCH_CONFIG = 600
+TOUCHE_ACTIVATION = 300
+touch_music = TouchPad(Pin(15))
+touch_OnOff = TouchPad(Pin(2))
+touch_music.config(TOUCH_CONFIG), touch_OnOff.config(TOUCH_CONFIG)
+
+led_aleatoire = Pin(21, Pin.OUT, 0)
+buzzer = PWM(Pin(27, Pin.OUT))
+buzzer.init(duty=0)
+timer = Timer(0)
+
 
 def musique():
     choix = musiques.random.choice(tuple(liste_music.values()))
@@ -27,17 +40,6 @@ def handleInterrupt(timer):
     elif touch_OnOff.read() <= TOUCHE_ACTIVATION and led_aleatoire.value() == 0:
         led_aleatoire.value(1)
         
-        
-TOUCH_CONFIG = 600
-TOUCHE_ACTIVATION = 300
-touch_music = TouchPad(Pin(15))
-touch_OnOff = TouchPad(Pin(2))
-touch_music.config(TOUCH_CONFIG), touch_OnOff.config(TOUCH_CONFIG)
-
-led_aleatoire = Pin(21, Pin.OUT, 0)
-buzzer = PWM(Pin(27, Pin.OUT))
-buzzer.init(duty=0)
-timer = Timer(0)
 
 timer.init(period=3000, mode=Timer.PERIODIC, callback=handleInterrupt)
 
